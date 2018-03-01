e7f948a3547c206a27505dfece3da3123583269b
knoldesparker <5735792+knoldesparker@users.noreply.github.com>
2018-03-01 22:16:09 +0100

    working SELECT and DELETE

---
+++ src/db.java
@@ -0,0 +1,47 @@
+import java.sql.*;
+
+public class db {
+    private String query;
+    private String dbUrl = "jdbc:mysql://localhost:3306/test";
+    private String username = "root";
+    private String password = "1234";
+
+    public void printUsers() {
+        query = "SELECT * FROM user";
+
+        try (Connection connection = DriverManager.getConnection(dbUrl,username,password);
+             Statement statement = connection.createStatement();
+             ResultSet rs = statement.executeQuery(query)){
+            System.out.println("Users");
+            while (rs.next()){
+                int iduser = rs.getInt(1);
+                String nameuser = rs.getString(2);
+                String passuser = rs.getString(3);
+
+                System.out.println("id# "+ iduser + "\t" + nameuser + "\t" + passuser);
+
+            }
+            connection.close();
+            rs.close();
+            statement.close();
+
+        } catch (SQLException e){
+            System.out.println(e.getMessage());
+        }
+
+    }
+    public void deleteUsers(){
+            String query = "DELETE FROM user WHERE nameuser = 'freja'";
+
+            try (Connection connection = DriverManager.getConnection(dbUrl,username,password);
+                 Statement statement = connection.createStatement()){
+                System.out.println(query);
+                statement.executeUpdate(query);
+
+            } catch (SQLException e){
+                System.out.println(e.getMessage());
+            }
+
+
+        }
+}
